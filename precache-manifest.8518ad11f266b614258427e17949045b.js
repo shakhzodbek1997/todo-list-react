8582088e0e4ec4d94d55f5fdfb487115a3941fab
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2019-12-03 15:11:51 +0500

    Updates

---
+++ precache-manifest.8518ad11f266b614258427e17949045b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd89147cedd94a792519c0e57801f3a",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "b2be761516b3c337e482",
+    "url": "/todo-list-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b1cdb6e4ed54fc0a73f5",
+    "url": "/todo-list-react/static/js/2.833f3d3b.chunk.js"
+  },
+  {
+    "revision": "b2be761516b3c337e482",
+    "url": "/todo-list-react/static/js/main.ab795447.chunk.js"
+  },
+  {
+    "revision": "52cbecb8c5b97360110f",
+    "url": "/todo-list-react/static/js/runtime-main.6b8219c5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todo-list-react/static/media/logo.25bf045c.svg"
+  }
+]);
